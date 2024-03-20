13f3d41a17d8eb0bca98b5ee3feb36ffcf9018cb
Josaphat UWIZEYE NGOGA <jngoga@g.hmc.edu>
2024-03-20 15:13:58 -0700

    GPS Plotting
    MATLAB code for plotting GPS Position data and map overaly

---
+++ MATLAB/GPS_Calibration.m
@@ -0,0 +1,60 @@
+% Based on logreader.m
+% Use this script to read data from your micro SD card
+
+clear;
+%clf;
+
+filenum = '003'; % file number for the data you want to read
+infofile = strcat('INF', filenum, '.TXT');
+datafile = strcat('LOG', filenum, '.BIN');
+
+%% map from datatype to length in bytes
+dataSizes.('float') = 4;
+dataSizes.('ulong') = 4;
+dataSizes.('int') = 4;
+dataSizes.('int32') = 4;
+dataSizes.('uint8') = 1;
+dataSizes.('uint16') = 2;
+dataSizes.('char') = 1;
+dataSizes.('bool') = 1;
+
+%% read from info file to get log file structure
+fileID = fopen(infofile);
+items = textscan(fileID,'%s','Delimiter',',','EndOfLine','\r\n');
+fclose(fileID);
+[ncols,~] = size(items{1});
+ncols = ncols/2;
+varNames = items{1}(1:ncols)';
+varTypes = items{1}(ncols+1:end)';
+varLengths = zeros(size(varTypes));
+colLength = 256;
+for i = 1:numel(varTypes)
+    varLengths(i) = dataSizes.(varTypes{i});
+end
+R = cell(1,numel(varNames));
+
+%% read column-by-column from datafile
+fid = fopen(datafile,'rb');
+for i=1:numel(varTypes)
+    %# seek to the first field of the first record
+    fseek(fid, sum(varLengths(1:i-1)), 'bof');
+    
+    %# % read column with specified format, skipping required number of bytes
+    R{i} = fread(fid, Inf, ['*' varTypes{i}], colLength-varLengths(i));
+    eval(strcat(varNames{i},'=','R{',num2str(i),'};'));
+end
+fclose(fid);
+
+%% Code to Plot the x,y positions overlaid on the campus map.
+% Read and Display img
+Campus_img = imread("C:\Users\josap\OneDrive\Documents\GitHub\E80-Final-Project\assets\img\GPS_test_2.png");
+figure(1);
+clf;
+imshow(Campus_img);
+hold on
+
+% Read and Plot x,y positions
+x = 1:1:20;
+y = 1:3:60;
+plot(x, y, 'bo', 'MarkerSize', 10);
+title(' x and y positions during the walk around Parsons');
